d12ffdede24efe6bac0f3b05f61a83bd4f9ecc56
iwataka123 <engineer.tk.hr@gmail.com>
2023-11-08 15:51:15 +0900

    修正4

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,7 +34,7 @@ class PrototypesController < ApplicationController
   def update
     prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
-      redirect_to edit_prototype_path
+      redirect_to prototype_path(prototype.id)
     else
       render :edit, satus: :unprocessable_entity
     end
